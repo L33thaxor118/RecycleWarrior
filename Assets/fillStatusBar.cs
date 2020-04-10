1c2137a90418ec3f5d1cae9f01039d24419307a8
Azriel Alvarado <aalvarado@fulcrumgt.com>
2020-04-10 17:39:13 -0400

    added menus

---
+++ Assets/fillStatusBar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class fillStatusBar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
