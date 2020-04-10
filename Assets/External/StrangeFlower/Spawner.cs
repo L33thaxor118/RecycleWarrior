06e94e54a463f0c59ff5efee1b7a829ad11df0f4
Azriel Alvarado <aalvarado@fulcrumgt.com>
2020-04-10 19:10:10 -0400

    cleaned up

---
+++ Assets/External/StrangeFlower/Spawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public Transform spawnPos;
+    public GameObject spawnee;
+    // Update is called once per frame
+    void Update()
+    {
+       
+        if(Input.GetKeyDown(KeyCode.P)) {
+            Instantiate(spawnee, new Vector3(spawnPos.position.x + 20, 0.62f, spawnPos.position.z), spawnPos.rotation);
+        }
+    }
+}
---
+++ Assets/Scripts/EatApple.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EatApple : MonoBehaviour
+{
+    public int end = 0;
+
+    public GameObject apple;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (end == 1) {
+            Destroy(apple);
+        }
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {   
+        Debug.Log(collision.gameObject.name);
+        if (collision.gameObject.name == "Player") {  
+            Debug.Log("eating");
+            end = 1;
+        }
+    }
+}
