a21509709f63c78a2bf0e609431556ee429a64cd
Azriel Alvarado <aalvarado@fulcrumgt.com>
2020-05-03 13:57:10 -0400

    fixed jump bug

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -33,6 +33,7 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(isGrounded);
 
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
