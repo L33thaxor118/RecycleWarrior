e03aea4470f14a06ab72b0327d2f4708d1c8e851
Azriel Alvarado <aalvarado@fulcrumgt.com>
2020-05-08 13:57:30 -0400

    added new terrains

--- Assets/Scripts/TerrainManager.cs
+++ Assets/Scripts/TerrainManager.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class TerrainManager : MonoBehaviour
 {   
     private int level;
+    public Transform grassManager;
     // Start is called before the first frame update
     void Start()
     {  
@@ -14,7 +16,13 @@ public class TerrainManager : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {     
+    {
+        if(Input.GetKeyDown(KeyCode.U)) {
+            this.level += 1;
+            Debug.Log(" Currently on Level 1");
+            Debug.Log(level);
+            LevelUp();
+        }        
     }
 
     public void LevelUp() {
@@ -31,6 +39,19 @@ public class TerrainManager : MonoBehaviour
         
         GameObject tnext = Terrain.CreateTerrainGameObject(next);
         tnext.transform.position = new Vector3(-50, 0 ,-50);
-       //Instantiate(tnext,new Vector3(-50,0,-50), Quaternion.identity);
+        var i = 0;
+        foreach (Transform item in grassManager)
+        {
+
+            GrowTree f = item.gameObject.GetComponent<GrowTree>();
+            i+= 1;
+            f.GrowUp();
+            if(i >= 3) {
+                break;
+            } 
+        }
+
+
+
     }
 }
---
+++ Assets/hfWork/GrowScene.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrowScene : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject tree;
+    public GameObject grass;
+    public TerrainManager terrainManager;
+    public GameObject seedsManager;
+
+    private float radius = 15.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(player.transform.position);
+    }
+
+    // Update is called once per frame
+    
+    void Update()
+    {   
+       // Debug.Log(this.radius);
+        Vector3 treeLocation = new Vector3(player.transform.position.x,player.transform.position.y, 0);
+        Vector3 playerLocation = new Vector3(tree.transform.position.x,tree.transform.position.y, 0);
+        if(Vector3.Distance(treeLocation, playerLocation) >= this.radius) {
+           
+           // Replace here with losing health function 
+        }
+        if(Input.GetKeyDown(KeyCode.E)) {
+            StartCoroutine(GrowBorder());
+        }
+        
+        
+    }
+
+    IEnumerator GrowGrass() { 
+        float currentTime = 0.0f; 
+        do
+        {
+            Debug.Log("growed grass");
+            currentTime += Time.deltaTime;
+            yield return null;
+        } while (currentTime <= 5);
+    }
+
+
+    IEnumerator GrowBorder() {
+        Vector3 originalScale = gameObject.transform.localScale;
+        Debug.Log(originalScale);
+        Vector3 destinationScale = new Vector3(originalScale.x + 30 , 100, originalScale.z + 30 );
+        this.radius += 15;
+         
+        float currentTime = 0.0f;
+         
+        do
+        {
+             gameObject.transform.localScale = Vector3.Lerp(originalScale, destinationScale, currentTime / 5);
+             currentTime += Time.deltaTime;
+             yield return null;
+        } while (currentTime <= 5);
+        terrainManager.LevelUp();
+         
+
+    }
+}
