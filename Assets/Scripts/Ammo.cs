75cf750659e216c6c67f628df4bf4717b0a13362
Yue <yuel7@DESKTOP-AOURJ12.localdomain>
2020-05-08 12:01:05 -0500

    adding scripts for ammo crosshear

---
+++ Assets/Scripts/Ammo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ammo : MonoBehaviour
+{
+    public int ammo;
+    public Text ammoDisplay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ammoDisplay.text = ammo.ToString();
+        if(Input.GetMouseButtonDown(0) && ammo > 0)
+        {
+            ammo--;
+        }
+    }
+}
---
+++ Assets/Scripts/Reticle.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Reticle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private RectTransform reticle;
+    public float restingSize;
+    public float maxSize;
+    public float speed;
+    private float currentSize;
+
+    private void Start()
+    {
+        reticle = GetComponent<RectTransform>();
+
+    }
+
+    private void Update()
+    {
+        if (isMoving)
+        {
+            currentSize = Mathf.Lerp(currentSize, maxSize, Time.deltaTime * speed);
+        }
+        else
+        {
+            currentSize = Mathf.Lerp(currentSize, restingSize, Time.deltaTime * speed);
+        }
+
+        reticle.sizeDelta = new Vector2(currentSize, currentSize);
+    }
+
+    bool isMoving
+    {
+        get
+        {
+            if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0) return true;
+            else return false;
+        }
+    }
+
+}
